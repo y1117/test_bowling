13464b810beca1be48ac3b33000973b745849345
Yunsub <sim861117@gmail.com>
2024-05-17 10:05:40 +0900

    [refactor] test code

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,9 +1,14 @@
 ﻿#include "pch.h"
 #include "../240517_bowling/bowling.cpp"
 
-TEST(GameTest, AllZeroPoints) {
+class GameFixture : public testing::Test {
+public:
     Game game;
-    for (int i = 0; i < 20; ++i) {
+};
+
+const int TOTAL_ROLL_TRY = 20;
+TEST_F(GameFixture, AllZeroPoints) {
+    for (int i = 0; i < TOTAL_ROLL_TRY; ++i) {
         game.Roll(0);
     }
     EXPECT_EQ(0, game.Score());
