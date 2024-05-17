5ada4a0d5d0fb3599b4f4b52c947b4073979b2ef
Yunsub <sim861117@gmail.com>
2024-05-17 10:43:54 +0900

    [feature] spare 1 others : all zero

--- 240517_bowling/bowling.cpp
+++ 240517_bowling/bowling.cpp
@@ -3,16 +3,45 @@ using namespace std;
 
 class Game {
 public:
+    Game() :
+        vPoints{ {} },
+        eachFramePoint{ 0 },
+        isFirstTry{ true },
+        isSpare { false}
+    {
+
+    }
     void Roll(int point) {
         vPoints.push_back(point);
     }
+
     int Score() {
         int result = 0;
-        for (int& point : vPoints) {
-            result += point;
+        for (auto it = vPoints.begin(); it != vPoints.end(); ++it) {
+            if (*it == 0) continue;
+
+            if (isFirstTry) {
+                if (isSpare) {
+                    result += *it;
+                    isSpare = false;
+                }
+                eachFramePoint = *it;
+                isFirstTry = false;
+            }
+            else {
+                eachFramePoint += *it;
+                if (eachFramePoint == 10) {
+                    isSpare = true;                    
+                }
+                isFirstTry = true;
+            }
+            result += *it;
         }
         return result;
     }
 private:
     vector<int> vPoints;
+    int eachFramePoint;
+    bool isFirstTry;
+    bool isSpare;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -19,6 +19,15 @@ TEST_F(GameFixture, All1Points) {
     }
     EXPECT_EQ(20, game.Score());
 }
+TEST_F(GameFixture, Spare1_OthersAllZeroPoints) {
+    game.Roll(5);
+    game.Roll(5);
+    game.Roll(3);
+    for (int i = 3; i < TOTAL_ROLL_TRY; ++i) {
+        game.Roll(0);
+    }
+    EXPECT_EQ(10+3+3, game.Score());
+}
 
 
 
