1e5b6dda5ca1b887316577da236ee5a725103277
Yunsub <sim861117@gmail.com>
2024-05-17 10:02:27 +0900

    [feature] All Zero Points

---
+++ 240517_bowling/bowling.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+using namespace std;
+
+class Game {
+public:
+    void Roll(int point) {
+        vPoints.push_back(point);
+    }
+    int Score() {
+        int result = 0;
+        for (int& point : vPoints) {
+            result += point;
+        }
+        return result;
+    }
+private:
+    vector<int> vPoints;
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,10 @@
-#include "pch.h"
+﻿#include "pch.h"
+#include "../240517_bowling/bowling.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(GameTest, AllZeroPoints) {
+    Game game;
+    for (int i = 0; i < 20; ++i) {
+        game.Roll(0);
+    }
+    EXPECT_EQ(0, game.Score());
 }
